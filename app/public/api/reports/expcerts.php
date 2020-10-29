7f05371b4139881c9641eeee414ee83f743442ef
ronnytacs <64437742+ronnytacs@users.noreply.github.com>
2020-10-29 13:03:54 -0400

    Expired Certification Table completed... Needs testing

---
+++ app/public/api/reports/expcerts.php
@@ -0,0 +1,31 @@
+<?php
+
+require 'common.php';
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+$sql = 'SELECT * FROM person p, receivedCerts rc, certification c
+        WHERE p.personID = rc.personID and
+        rc.certID = c.certID and
+        rc.dateExpired != "0000-00-00"';
+$vars = [];
+
+if (isset($_GET['personID'])) {
+ // This is an example of a parameterized query
+$sql = 'SELECT * FROM person WHERE personID = ?';
+$vars = [ $_GET['personID'] ];
+}
+
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
+
+$person = $stmt->fetchAll();
+
+// Step 3: Convert to JSON
+$json = json_encode($person, JSON_PRETTY_PRINT);
+
+// Step 4: Output
+header('Content-Type: application/json');
+echo $json;
--- app/public/js/report.app.js
+++ app/public/js/report.app.js
@@ -85,4 +85,52 @@ new Vue({
   data: {
     checkedNames: [],
   }
-})
+});
+
+var reportApp = new Vue({
+  el: '#expiredVue',
+  data: {
+    person: [{
+      personID:'',
+      firstName: '',
+      lastName:'',
+      address:'',
+      city:'',
+      state:'',
+      zip:'',
+      workPhone:'',
+      mobilePhone:'',
+      homePhone:'',
+      dateOfBirth:'',
+      Gender:'',
+      Position:'',
+      radioNumber:'',
+      stationNumber: '',
+      isActive:'',
+      email: '',
+      startDate:'',
+      department:'',
+      recievedID: '',
+      certID: '',
+      dateRecieved: '',
+      dateExpired: '',
+      certName: '',
+      certAgency: '',
+      standardExpiry: ''
+    }]
+  },
+  methods: {
+    fetchPerson(){
+      fetch('api/reports/expcerts.php')
+        .then( response => response.json() )
+        .then( json => {
+          this.person = json;
+          console.log(this.person);
+        });
+    }
+  },
+  created(){
+    this.fetchPerson();
+    console.log("fetched");
+  }
+});
