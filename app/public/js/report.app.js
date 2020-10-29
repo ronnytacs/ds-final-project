fba3aae1397a8388cd48379be00914f5c6b0e342
ronnytacs <64437742+ronnytacs@users.noreply.github.com>
2020-10-28 23:22:20 -0400

    V-For works Tables populated filters still not working

--- app/public/js/report.app.js
+++ app/public/js/report.app.js
@@ -1,7 +1,88 @@
+var reportApp = new Vue({
+  el: '#stationNumberVue',
+  data: {
+    person: [{
+      personID:'',
+      firstName: '',
+      lastName:'',
+      address:'',
+      city:'',
+      state:'',
+      zip:'',
+      workPhone:'',
+      mobilePhone:'',
+      homePhone:'',
+      dateOfBirth:'',
+      Gender:'',
+      Position:'',
+      radioNumber:'',
+      stationNumber: '',
+      isActive:'',
+      email: '',
+      startDate:'',
+      department:'',
+    }]
+  },
+  methods: {
+    fetchPerson(){
+      fetch('api/reports/index.php')
+        .then( response => response.json() )
+        .then( json => {
+          this.person = json;
+          console.log(this.person);
+        });
+    }
+  },
+  created(){
+    this.fetchPerson();
+    console.log("fetched");
+  }
+});
+
+var reportNum = new Vue({
+  el: '#radioNumberVue',
+  data: {
+    person: [{
+      personID:'',
+      firstName: '',
+      lastName:'',
+      address:'',
+      city:'',
+      state:'',
+      zip:'',
+      workPhone:'',
+      mobilePhone:'',
+      homePhone:'',
+      dateOfBirth:'',
+      Gender:'',
+      Position:'',
+      radioNumber:'',
+      stationNumber: '',
+      isActive:'',
+      email: '',
+      startDate:'',
+      department:'',
+    }]
+  },
+  methods: {
+    fetchPerson(){
+      fetch('api/reports/index.php')
+        .then( response => response.json() )
+        .then( json => {
+          this.person = json;
+          console.log(this.person);
+        });
+    }
+  },
+  created(){
+    this.fetchPerson();
+    console.log("fetched");
+  }
+});
 
 new Vue({
-  el: '#example-5',
+  el: '#radioNumFilter',
   data: {
-    selected: ''
+    checkedNames: [],
   }
 })
